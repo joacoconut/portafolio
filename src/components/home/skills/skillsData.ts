0f9fba4e465f7a07859ebe2fa8be9b824d9ec1e9
Joaquin Vega <joaquinvega241200@gmail.com>
2025-03-30 00:48:02 -0300

    tecnologias e icono actualizado

--- src/components/home/skills/skillsData.ts
+++ src/components/home/skills/skillsData.ts
@@ -7,6 +7,17 @@ export const skills = [
     title: "CSS3",
     src: "https://img.icons8.com/color/96/000000/css3.png",
   },
+
+  {
+    title: "SASS",
+    src: "https://img.icons8.com/color/96/000000/sass-avatar.png",
+  },
+
+  {
+    title: "TailwindCSS",
+    src: "https://img.icons8.com/color/96/000000/tailwindcss.png",
+  },
+
   {
     title: "JavaScript",
     src: "https://img.icons8.com/color/96/000000/javascript--v1.png",
@@ -15,41 +26,41 @@ export const skills = [
     title: "ReactJS",
     src: "https://img.icons8.com/color/96/000000/react-native.png",
   },
-  {
-    title: "NextJS",
-    src: "https://img.icons8.com/color/96/000000/nextjs.png",
-  },
-  {
-    title: "Angular",
-    src: "https://img.icons8.com/?size=100&id=l9a5tcSnBwcf&format=png&color=000000",
-  },
+
   {
     title: "TypeScript",
     src: "https://img.icons8.com/color/96/000000/typescript.png",
   },
-  {
-    title: "SASS",
-    src: "https://img.icons8.com/color/96/000000/sass-avatar.png",
-  },
-  {
-    title: "Bootstrap",
-    src: "https://img.icons8.com/?size=100&id=g9mmSxx3SwAI&format=png&color=000000",
-  },
 
   {
+    title: "NextJS",
+    src: "https://img.icons8.com/color/96/000000/nextjs.png",
+  },
+  /*  {
+    title: "Angular",
+    src: "https://img.icons8.com/?size=100&id=l9a5tcSnBwcf&format=png&color=000000",
+  }, */
+
+  /*  {
+    title: "Bootstrap",
+    src: "https://img.icons8.com/?size=100&id=g9mmSxx3SwAI&format=png&color=000000",
+  }, */
+
+  /* {
     title: "Material UI",
     src: "https://img.icons8.com/color/96/000000/material-ui.png",
   },
   {
     title: "Chakra UI",
     src: "https://img.icons8.com/color/96/000000/chakra-ui.png",
-  },
+  }, */
+
   {
     title: "Git & GitHub",
     src: "https://img.icons8.com/external-tal-revivo-color-tal-revivo/96/000000/external-github-with-cat-logo-an-online-community-for-software-development-logo-color-tal-revivo.png",
   },
-  {
+  /*  {
     title: "Firebase",
     src: "https://img.icons8.com/?size=100&id=62452&format=png&color=000000",
-  },
+  }, */
 ];
